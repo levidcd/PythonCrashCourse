0bbddcfce13a7e5e14e55bf23337cc4f28c379e7
LeiWang <levidcd@outlook.com>
2020-10-05 15:43:15 +0800

    添加新测试

--- chapter 11/test_name_function.py
+++ chapter 11/test_name_function.py
@@ -13,13 +13,19 @@
 import unittest
 from name_function import get_formatted_name
 
+
 class NamesTestCase(unittest.TestCase):
     """测试 name_function.py"""
 
     def test_first_last_name(self):
         """"""
-        formatted_name = get_formatted_name('jains','joplin')
-        self.assertEqual(formatted_name,'Jains Joplin')
+        formatted_name = get_formatted_name('jains', 'joplin')
+        self.assertEqual(formatted_name, 'Jains Joplin')
+
+    def test_first_last_middle_name(self):
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 
 if __name__ == '__main__':
     unittest.main()
