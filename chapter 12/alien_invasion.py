c0c9e925ad808a6b3c2eddbf6dab6b9e27a0a847
LeiWang <levidcd@outlook.com>
2020-10-05 17:23:48 +0800

    初始化游戏面板

---
+++ chapter 12/alien_invasion.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   alien_invasion.py
+@Time    :   2020/10/05 17:15:38
+@Author  :   
+@Version :   1.0
+@Contact :   
+@WebSite :   
+@Desc    :          
+'''
+# Start typing your code from here
+import sys
+import pygame
+
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('Alien Invasion')
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
