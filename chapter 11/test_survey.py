a55f769cfda7d8108205d5311b1d7dd86468a110
LeiWang <levidcd@outlook.com>
2020-10-05 16:51:38 +0800

    完成测试

--- chapter 11/test_survey.py
+++ chapter 11/test_survey.py
@@ -3,32 +3,34 @@
 '''
 @File    :   test_survey.py
 @Time    :   2020/10/05 16:10:24
-@Author  :   
+@Author  :
 @Version :   1.0
-@Contact :   
-@WebSite :   
-@Desc    :          
+@Contact :
+@WebSite :
+@Desc    :
 '''
 # Start typing your code from here
 import unittest
 from survey import AnonymousSurvey
 
+
 class TestAnonymousSurvey(unittest.TestCase):
-    def test_store_single_response(self):
+    def setUp(self):
         question = "language?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.response)
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['a', 'b', 'c']
+
+    def test_store_single_response(self):
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.response)
 
     def test_store_three_response(self):
-        question = "language?"
-        my_survey = AnonymousSurvey(question)
-        responses = ['a', 'b', 'c']
-        for response in responses:
-            my_survey.store_response(response)
+        for response in self.responses:
+            self.my_survey.store_response(response)
 
-        for response in responses:
-            self.assertIn(response, my_survey.response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.response)
+
 
 if __name__ == '__main__':
     unittest.main()
