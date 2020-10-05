8fd68020ab146b0dcfc6ff610c80cc5e0cb186b4
LeiWang <levidcd@outlook.com>
2020-10-05 20:36:50 +0800

    限制子弹数量

--- chapter 12/alien_invasion/alien_invasion.py
+++ chapter 12/alien_invasion/alien_invasion.py
@@ -77,8 +77,9 @@ class AlienInvasion:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
     def _update_screen(self):
