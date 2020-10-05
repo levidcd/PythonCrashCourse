adab404d7083079339096b6d26901e4c8cce238c
LeiWang <levidcd@outlook.com>
2020-10-05 15:12:15 +0800

    第十一章

--- chapter 10/exercise/10_5.py
+++ chapter 10/exercise/10_5.py
@@ -1,19 +1,18 @@
 #!/usr/local/bin/python3
 # -*- encoding: utf-8 -*-
 '''
-@File    :   10_4.py
-@Time    :   2020/10/03 14:06:12
-@Author  :
+@File    :   10_5.py
+@Time    :   2020/10/03 14:31:25
+@Author  :   
 @Version :   1.0
-@Contact :
-@WebSite :
-@Desc    :   写入多行
+@Contact :   
+@WebSite :   
+@Desc    :          
 '''
 # Start typing your code from here
+filename = 'programming.txt'
 
-filename = 'guest_book.txt'
-
-prompt = 'What is your name:\n'
+prompt = 'Why you like programming:\n'
 prompt += "(Enter 'quit' when you are finished.)\n"
 while True:
     name = input(prompt)
@@ -23,4 +22,3 @@ while True:
     else:
         with open(filename, 'a') as file_object:
             file_object.write(f"{name}\n")
-            print(f'welcome!{name}.')
