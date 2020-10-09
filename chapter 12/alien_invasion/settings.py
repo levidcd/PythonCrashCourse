ad4c0b4f1a2cdc3763d030a88f008cf8e52499fc
LeiWang <levidcd@outlook.com>
2020-10-09 15:31:08 +0800

    外星人移动

--- chapter 12/alien_invasion/settings.py
+++ chapter 12/alien_invasion/settings.py
@@ -19,6 +19,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        
+        # 1 right  -1 left 
+        self.fleet_direction = 1
 
         self.bullet_speed = 1.0
         self.bullet_width = 3
